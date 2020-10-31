2b9cb089b206c910b3771123d5fb67f2a6cb690c
András Parditka <andraaspar@gmail.com>
2020-10-31 09:33:04 +0100

    Updates

---
+++ precache-manifest.46215c4e27b8f3102eb80ea82894ad2a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc7c48529ba18e8fcf094098f14d65c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "375a4f9761aa86ad50eb",
+    "url": "/todo/static/css/main.6ee82f28.chunk.css"
+  },
+  {
+    "revision": "5d91e5597b42b742bc1b",
+    "url": "/todo/static/js/2.7c202403.chunk.js"
+  },
+  {
+    "revision": "3e6933cf6bd44f8f91c7aa725d75c668",
+    "url": "/todo/static/js/2.7c202403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375a4f9761aa86ad50eb",
+    "url": "/todo/static/js/main.9422c196.chunk.js"
+  },
+  {
+    "revision": "10fd3631c43d335a3db9",
+    "url": "/todo/static/js/runtime-main.2a8ec137.js"
+  },
+  {
+    "revision": "ca0bfca035935e7fc839bcedc24f8eb8",
+    "url": "/todo/static/media/arrow-down-short.ca0bfca0.svg"
+  },
+  {
+    "revision": "7ee4829c34a84d4f131323028ae8977d",
+    "url": "/todo/static/media/arrow-left-short.7ee4829c.svg"
+  },
+  {
+    "revision": "fad9db7fbf194c67d586fddbd4bd95b3",
+    "url": "/todo/static/media/arrow-right-short.fad9db7f.svg"
+  },
+  {
+    "revision": "ab114b6787b50ba1ba5b5af6390e6601",
+    "url": "/todo/static/media/arrow-up-down.ab114b67.svg"
+  },
+  {
+    "revision": "9f7e028851bd19b5af815416f7c2d1a6",
+    "url": "/todo/static/media/arrow-up-short.9f7e0288.svg"
+  },
+  {
+    "revision": "70748ef05245524594c9751133a942ae",
+    "url": "/todo/static/media/check-circle.70748ef0.svg"
+  },
+  {
+    "revision": "d0ff65dd1e2a1b51ca9df5ddde48a358",
+    "url": "/todo/static/media/check.d0ff65dd.svg"
+  },
+  {
+    "revision": "a7e1dff47a5a72025d165dbf9fe05b91",
+    "url": "/todo/static/media/circle.a7e1dff4.svg"
+  },
+  {
+    "revision": "30a66ff1f0d119c581920bb474bd2400",
+    "url": "/todo/static/media/plus.30a66ff1.svg"
+  },
+  {
+    "revision": "7bfd8c50627a744684d62e576f2878b0",
+    "url": "/todo/static/media/trash.7bfd8c50.svg"
+  },
+  {
+    "revision": "9701d9b5f0def6c16cfdeb5824885875",
+    "url": "/todo/static/media/x-circle.9701d9b5.svg"
+  }
+]);
